eab45ac7e3f562eeef3a3864d18a149461e1553f
Duminoi <dminso1koaiso2@gmail.com>
2024-05-11 09:39:19 +0700

    git push

---
+++ Day-21/hw/js/hw.js
@@ -0,0 +1,12 @@
+//-------------- variable b1------------------
+var btn1 = document.getElementById("btn1");
+function bai1{
+    var arr = [2,3,5,4,6,7,1];
+    var max;
+    var maxIndex;
+    for(var index in arr ){
+        if(max > index){
+            max =
+        }
+    }
+}
