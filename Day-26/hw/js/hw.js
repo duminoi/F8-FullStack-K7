ae7db756b1c13a8b03568ccb5cc2644ff737a134
duminoi <dminso1koaiso2@gmail.com>
2024-05-28 21:02:49 +0700

    hw

--- Day-26/hw/js/hw.js
+++ Day-26/hw/js/hw.js
@@ -18,8 +18,50 @@ Object.prototype.getCurrency = function (unit) {
   var unitAbs = unit.split("").filter(function (value) {
     return value !== " ";
   });
-  var number = Number(price).toLocaleString() + " " + unitAbs;
+  var number = Number(this).toLocaleString() + " " + unitAbs;
   return number;
 };
 
-console.log(price.getCurrency("    đ  "));
+console.log(price.getCurrency(" đ  "));
+// -----------------------------------------------------------
+
+console.log("Bài 3");
+
+Array.prototype.push2 = function (...args) {
+  for (let value of args) {
+    this[this.length] = value;
+  }
+  return this;
+};
+
+var arr1 = [];
+arr1.push2(1, 2);
+console.log(arr1);
+
+// -----------------------------------------------------------
+console.log("Bài 4");
+Array.prototype.filter2 = function (callback) {
+  var newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (
+      callback(this[i], i) !== false &&
+      callback(this[i], i) !== 0 &&
+      callback(this[i], i) !== -0 &&
+      callback(this[i], i) !== "" &&
+      callback(this[i], i) !== null &&
+      callback(this[i], i) !== undefined &&
+      callback(this[i], i) !== NaN
+    ) {
+      newArr[newArr.length] = this[i];
+    }
+  }
+  return newArr;
+};
+var arr2 = [1, 2, 3, 4, 5, 6];
+var newArr = arr2.filter2(function (value) {
+  return value % 2 == 0;
+});
+console.log(newArr);
+// -----------------------------------------------------------
+
+console.log("Bài 5");
