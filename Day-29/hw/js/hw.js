6eec0e5fee7685f62090bd48051a9e348bc0447b
duminoi <dminso1koaiso2@gmail.com>
2024-06-09 00:51:00 +0700

    update

--- Day-29/hw/js/hw.js
+++ Day-29/hw/js/hw.js
@@ -1,12 +1,26 @@
 var prev = document.querySelector(".slide-nav .prev");
 var next = document.querySelector(".slide-nav .next");
 var item = document.querySelectorAll(".item img");
-var dots = document.querySelectorAll(".dot");
+
+var slideDots = document.querySelector(".slide-dots");
+
 var sliderInner = document.querySelector(".slide-inner");
 var sliderInnerWidth = item[0].clientWidth * item.length;
 var position = 0;
 var index = 0;
+var initX = 0;
+var lastPosition = 0;
+//create dot
+for (let i = 0; i < item.length; i++) {
+  var dot = document.createElement("div");
+  dot.classList.add("dot");
+  slideDots.appendChild(dot);
+}
+// active dot
+var dots = document.querySelectorAll(".dot");
+
 dots[index].classList.add("active");
+
 function handleClick(direction) {
   if (direction === 1) {
     if (Math.abs(position) < item[0].clientWidth * (item.length - 1)) {
@@ -44,7 +58,7 @@ function handleClick(direction) {
     dots[index].classList.add("active");
   }
 
-  console.log(index);
+  console.log(position);
 }
 prev.addEventListener("click", function () {
   handleClick(-1);
@@ -56,8 +70,9 @@ next.addEventListener("click", function () {
 //press dot to swipe
 dots.forEach(function (dot, index) {
   dot.addEventListener("click", function () {
+    position = index * item[0].clientWidth;
     var css = {
-      transform: `translateX(-${index * item[0].clientWidth}px)`,
+      transform: `translateX(-${position}px)`,
       transition: ` 0.3s ease-in-out`,
     };
     var dotActive = document.querySelectorAll(".slide-dots .dot.active");
@@ -68,32 +83,51 @@ dots.forEach(function (dot, index) {
     dot.classList.add("active");
 
     Object.assign(sliderInner.style, css);
+    lastPosition = position;
+    console.log(position);
   });
 });
-var position = 0;
-// var swipe;
-var initX = 0;
-sliderInner.addEventListener("mousedown", function (e) {
+var swipe;
+function handleDown(e) {
   e.preventDefault();
-  initX = e.offsetX;
+  initX = e.clientX;
   console.log("initX", initX);
   sliderInner.addEventListener("mousemove", handleMove);
   sliderInner.addEventListener("mouseup", function () {
+    if (swipe < 150) {
+      sliderInner.style.transform = `translateX(-${lastPosition}px)`;
+    }
     sliderInner.removeEventListener("mousemove", handleMove);
+    lastPosition = position;
+    console.log("lastPosit", lastPosition);
+    sliderInner.style.cursor = "default";
   });
-});
+}
 
 function handleMove(e) {
-  var swipe = e.offsetX - initX;
-  position = position + swipe;
-  if (swipe) {
-    sliderInner.style.transform = `translateX(${position}px)`;
-    if (Math.abs(position) === 300) {
-      position = position + item[0].clientWidth;
-      sliderInner.style.transform = `translateX(${position}px)`;
+  swipe = initX - e.clientX;
+  sliderInner.style.cursor = "move";
+  sliderInner.style.transition = "transform 0.3s ease";
+  if (swipe > 0) {
+    if (swipe < 150) {
+      position = lastPosition + swipe;
+      // if(position <)
+      sliderInner.style.transform = `translateX(-${position}px)`;
+    } else {
+      position = lastPosition + item[0].clientWidth;
+      sliderInner.style.transform = `translateX(-${position}px)`;
+      sliderInner.style.cursor = "default";
     }
+  } else {
+    position = lastPosition + swipe;
+
+    sliderInner.style.transform = `translateX(-${position}px)`;
   }
-  // console.log("initX", initX);
-  // console.log("e.offset", e.clientX);
-  // console.log("swipe", swipe);
+  // lastPosition = position;
+  console.log("initX", initX);
+  console.log("e.offset", e.clientX);
+  console.log("swipe", swipe);
+  console.log("position", position);
 }
+
+sliderInner.addEventListener("mousedown", handleDown);
