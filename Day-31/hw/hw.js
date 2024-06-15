def25849aee20a5f4a1f2f9776e0156edb1e1d56
duminoi <dminso1koaiso2@gmail.com>
2024-06-15 16:55:27 +0700

    update

--- Day-31/hw/hw.js
+++ Day-31/hw/hw.js
@@ -12,6 +12,13 @@ var lastOffsetProgressBar = 0;
 var offsetProgressBar = 0;
 var currentTimeBar = 0;
 
+var audio = document.querySelector(".audio");
+var player = document.querySelector(".player");
+var playerTimer = player.querySelector(".play-timer");
+var playBtn = document.querySelector(".play-btn i");
+var currentTimeEl = playerTimer.firstElementChild;
+var durationEl = playerTimer.lastElementChild;
+var duration = 0;
 //Bấm chuột xuống tại progress-bar
 progressBar.addEventListener("mousedown", function (e) {
   //Lấy được tọa độ tại vị trí bấm (offsetX)
@@ -37,7 +44,11 @@ progressSpan.addEventListener("mousedown", function (e) {
 var handleTimeUpdate = function () {
   var currentTime = audio.currentTime;
   currentTimeEl.innerHTML = getTimeFormat(currentTime);
-  var rate = (currentTime / duration) * 100;
+  var rate = (currentTime / audio.duration) * 100;
+  if (currentTime == audio.duration) {
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.addEventListener("click", function () {});
+  }
   progress.style.width = `${rate}%`;
 };
 //Khi nhả chuột
@@ -62,17 +73,11 @@ var handleDrag = function (e) {
   audio.removeEventListener("timeupdate", handleTimeUpdate);
   progress.style.width = rate + "%";
   currentTimeBar = (audio.duration * rate) / 100;
+  audio.play();
   console.log("currentTimeBar", currentTimeBar);
 };
+// ------------------------------------------------Xử lý audio
 
-var audio = document.querySelector(".audio");
-var player = document.querySelector(".player");
-var playerTimer = player.querySelector(".play-timer");
-var playBtn = document.querySelector(".play-btn i");
-var currentTimeEl = playerTimer.firstElementChild;
-var durationEl = playerTimer.lastElementChild;
-
-var duration = 0;
 var setDuration = function () {
   duration = audio.duration;
 }; // lấy thời gian của audio
