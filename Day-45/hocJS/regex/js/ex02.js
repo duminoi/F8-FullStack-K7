169778c107ff13a593269b0ff2b1e4429414d13b
duminoi <dminso1koaiso2@gmail.com>
2024-08-03 00:10:11 +0700

    u

--- Day-45/hocJS/regex/js/ex02.js
+++ Day-45/hocJS/regex/js/ex02.js
@@ -15,8 +15,8 @@
 // const pattern = /^(0|\+84)[0-9]{9}$/;
 // console.log(pattern.test(phone));
 
-// const str =`Hoangan@!#$12345`
-// const pattern = /[^a-zA-Z0-9]/
+// const str = `Hoangan@!#$12345`;
+// const pattern = /[^a-zA-Z0-9]/;
 
 /* bài 1
 kiểm tra username hợp lệ:
@@ -45,7 +45,7 @@ username@domain.ext
 - Tối thiểu 2 ký tự
 
 Test:
-hoangan.web-@gmail.com ==> failed
+hoangan.web-@gmail.com ==> failed (Trước @ không được là kí tự đặc biệt như '-' hay '_')
 hoangan@fullstack-.edu.vn ==> failed
 
 */
@@ -55,28 +55,37 @@ hoangan@fullstack-.edu.vn ==> failed
 //   /^[a-zA-Z][a-zA-Z0-9-_.]+[a-zA-Z0-9]@([a-zA-Z]|[a-zA-Z0-9-_]*[a-zA-Z0-9])\.([a-zA-Z0-9-_]*[a-zA-Z0-9]\.)*[a-zA-Z]{2,}$/;
 // console.log(pattern.test(tenMien));
 
-//Cắt chuỗi
+/*
+Cắt chuỗi
+*/
 // const content = `Xin chao anh em, tôi la 0378096074 abc xtz 0123456788`;
 // const pattern = /(0|\+84)\d{9}/g;
 // const phone = content.match(pattern);
 // console.log(phone);
 
-//Capturing the Group: Kỹ thuật chụp lại 1 phần của biểu thức dể trả về kết quả. Thêm cặp ngoặc ()
-// ==> Lưu ý: Không hỗ trợ với global
+/*
+Capturing the Group: Kỹ thuật chụp lại 1 phần của biểu thức dể trả về kết quả. Thêm cặp ngoặc ()
+- Sử dụng "?:" để non-capturing group(không lấy kết quả của phần ngoặc tròn đó)
+==> Lưu ý: Không hỗ trợ với global
+*/
 // const email = `ducminh.web@nodejs.fullstack.edu.vn`;
 // const pattern =
 //   /^([a-zA-Z][a-zA-Z0-9-_.]+[a-zA-Z0-9])@(?:[a-zA-Z]|[a-zA-Z0-9-_]*[a-zA-Z0-9])\.(?:[a-zA-Z0-9-_]*[a-zA-Z0-9]\.)*[a-zA-Z]{2,}$/;
 // const result = email.match(pattern);
 // console.log(result);
 
-//lazy,greedy
+/*
+lazy,greedy
+*/
 
-//Thay thế
+/*
+Thay thế
+*/
 // const content =
 //   "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop dminhello@gmail.com packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 
 // const pattern =
-//   /[a-zA-Z][a-zA-Z0-9-_.]+[a-zA-Z0-9]@([a-zA-Z]|[a-zA-Z0-9-_]*[a-zA-Z0-9])\.([a-zA-Z0-9-_]*[a-zA-Z0-9]\.)*[a-zA-Z]{2,}/g;
+//   /(([a-zA-Z][a-zA-Z0-9-_.]+[a-zA-Z0-9])@(?:[a-zA-Z]|[a-zA-Z0-9-_]*[a-zA-Z0-9])\.(?:[a-zA-Z0-9-_]*[a-zA-Z0-9]\.)*[a-zA-Z]{2,})/;
 
 // const result = content.replace(
 //   pattern,
@@ -84,14 +93,15 @@ hoangan@fullstack-.edu.vn ==> failed
 // );
 // document.body.innerHTML = result;
 
-// Đối sánh chuỗi: Chụp lại kết quả của biểu thức để đưa vào giá trị sau khi thay thế (Dựa vào capturing group)
-
+/*
+Đối sánh chuỗi: Chụp lại kết quả của biểu thức để đưa vào giá trị sau khi thay thế (Dựa vào capturing group)
+ */
 // group 1 ==> $1
 // group 2 ==> $2
 // group 3 ==> $3
 // group 4 ==> $4
 
-let url = "http://fullstack.edu.vn////";
-const pattern = /\/+$/g;
-url = url.replace(pattern, "");
-console.log(url);
+// let url = "http://fullstack.edu.vn////";
+// const pattern = /\/+$/g;
+// url = url.replace(pattern, "");
+// console.log(url);
