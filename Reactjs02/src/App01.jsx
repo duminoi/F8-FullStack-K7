acd9d0ca23a17723c76db2a1181b3b45ca79fab6
duminoi <dminso1koaiso2@gmail.com>
2024-08-24 01:01:12 +0700

    u

---
+++ Reactjs02/src/App01.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import Check from "./components/Check";
+// import Counter from "./components/Counter";
+// import HelloWorld from "./components/HelloWorld";
+
+export default function App() {
+  return (
+    <div>
+      {/* <HelloWorld name="Đức Minh" age={32} /> */}
+      {/* <Counter /> */}
+      <Check />
+    </div>
+  );
+}
